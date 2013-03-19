d77330c20b335650d402dbd6a486c9b0aa904309
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2013-03-19 21:48:56 +0200

    Add solution to 002

---
+++ 002.lisp
@@ -0,0 +1,14 @@
+(defun next-fibonacci (l)
+  (+ (nth (- (length l) 1) l)
+     (nth (- (length l) 2) l)))
+
+(defun fibonaccies-smaller-than (n generated)
+  (if (> (next-fibonacci generated) n)
+    generated
+    (fibonaccies-smaller-than n (append generated (list (next-fibonacci generated))))))
+
+(defun generate-fibonaccies-smaller-than (n)
+  (fibonaccies-smaller-than n '(1 1)))
+
+(defun solve (n)
+  (apply #'+ (remove-if-not #'evenp (generate-fibonaccies-smaller-than n))))
