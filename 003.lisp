37b2e16a424ef75422c7185302edf59426b4a905
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2013-04-08 18:56:33 +0300

    Optimize 003 prime factor search

--- 003.lisp
+++ 003.lisp
@@ -7,5 +7,6 @@
 (defun factor (n k)
   (cond ((> k (floor (sqrt n))) (list n))
         ((= (mod n k) 0) (cons k (factor (truncate n k) 2)))
-        (t (factor n (+ k 1)))))
+        ((= k 2) (factor n 3))
+        (t (factor n (+ k 2)))))
 
