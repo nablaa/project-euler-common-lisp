524d415fc4c40ec884dcc6d125bd6496e00fe3c6
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2013-04-09 21:11:33 +0300

    Add another solution to 010
    This solution uses the sieve of Erastothenes. It is faster, but
    not as elegant as the first solution.

--- 010.lisp
+++ 010.lisp
@@ -1,3 +1,5 @@
+; Elegant, but slow
+
 (defun sum-primes-smaller-than (n)
   (loop for x from 2 to (- n 1) when (prime? x) sum x))
 
@@ -9,3 +11,20 @@
         ((= (mod n k) 0) nil)
         ((= k 2) (is-prime n 3))
         (t (is-prime n (+ k 2)))))
+
+
+; Ugly, but fast
+
+(defun sum-sieve (n)
+  (reduce #'+ (remove-if #'null (sieve n))))
+
+(defun sieve (n)
+  (let ((numbers (make-array (- n 2))))
+    (loop for x from 2 to (- n 1) do (setf (aref numbers (- x 2)) x))
+    (loop for x from 0 to (- n 3) do (let ((elem (aref numbers x)))
+                                       (when (not (null elem))
+                                         (setq index (+ x elem))
+                                         (loop while (< index (length numbers)) do
+                                               (setf (aref numbers index) nil)
+                                               (setf index (+ index elem))))))
+    numbers))
