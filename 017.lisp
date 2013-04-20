da8c685e48dad365424a9f044798ce2586aec668
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2013-04-20 11:14:50 +0300

    Add solution to 017

---
+++ 017.lisp
@@ -0,0 +1,11 @@
+(defun total-letter-count (max-n)
+  (loop :for n :from 1 :to max-n :sum (letter-count n)))
+
+(defun letter-count (n)
+  (length (filter-chars (number-to-string n))))
+
+(defun filter-chars (s)
+  (remove #\- (remove #\space s)))
+
+(defun number-to-string (n)
+  (format nil "~R" n))
