dbf3fb6eb220c67fec12365a8ca26ae44c464594
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2013-04-20 10:21:51 +0300

    Simplify 014

--- 014.lisp
+++ 014.lisp
@@ -1,27 +1,19 @@
 (defun solve ()
-  (longest-chain-under-n 1000000 #'chain-length-hash))
+  (longest-chain-under-n 1000000))
 
-(defun longest-chain-under-n (n func)
+(defun longest-chain-under-n (n)
   (let ((max-value 0)
         (index 1))
-    (loop :for i :from 1 :below n :if (> (funcall func i) max-value)
-          :do (setf max-value (funcall func i)) (setf index i))
+    (loop :for i :from 1 :below n :if (> (chain-length i) max-value)
+          :do (setf max-value (chain-length i)) (setf index i))
     index))
 
-(defun chain-length (n)
-  (chain-length-acc n 0))
-
-(defun chain-length-acc (n acc)
-  (if (= n 1)
-    (+ acc 1)
-    (chain-length-acc (collatz n) (+ acc 1))))
-
 (let ((hash (make-hash-table)))
-  (defun chain-length-hash (n)
+  (defun chain-length (n)
     (if (= n 1)
       1
       (if (null (gethash n hash))
-        (setf (gethash n hash) (+ 1 (chain-length-hash (collatz n))))
+        (setf (gethash n hash) (+ 1 (chain-length (collatz n))))
         (gethash n hash)))))
 
 (defun collatz (n)
